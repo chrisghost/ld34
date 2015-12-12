2eca7cb12ba136816832716714ce42d63b5cc54c
chrisghost <maillol.adrien@gmail.com>
2015-12-12 15:22:23 +0100

    wip - walls draw

--- Main.elm
+++ Main.elm
@@ -9,51 +9,60 @@ import Debug exposing (..)
 
 -- MODEL
 
-type alias Model =
-  { x : Float
-  , y : Float
-  , vx : Float
+type alias Positioned a = { a | x : Float, y : Float }
+
+type alias Ship =
+  Positioned {
+    vx : Float
   , vy : Float
   , a : Float
   }
 
+type alias Wall = Positioned { w: Int, h: Int }
 
-ship : Model
-ship =
-  { x = 0
-  , y = 0
-  , vx = 0
-  , vy = 0
-  , a = 0
+type alias Model =
+  {
+    walls : List Wall,
+    ship: Ship
   }
 
+
+model : Model
+model = Model [ {x= -100, y=100, w= 500, h= 20 } ] {x=0, y=0, vx=0, vy=0, a=0}
+--model = Model [ Wall { -100 100 500 20} ] {x=0, y=0, vx=0, vy=0, a=0}
 
 type alias Keys = { x: Int, y: Int }
 
 -- UPDATE
 
---getvecship : Model -> String
+getvecship : Ship -> (Float, Float)
 getvecship ship = (-(sin ship.a), (cos ship.a))
 
-update : (Float, Keys) -> Model -> Model
-update (dt, keys) ship =
-  let 
-    vec = (getvecship ship)
+--update : (Float, Keys) -> Model -> Model
+update (dt, keys) model =
+  let
+    vec = getvecship model.ship
     --_ = log "> " vec
-  in 
-    ship
+    ship' = model.ship
       |> rotateShip keys.x dt
       |> thrust keys.y dt
-      |> physics dt
+      |> physics model.ship.vx 0 dt
+
+    walls' = List.map (physics 0 -model.ship.vy dt) model.walls
+  in
+    { model |
+      walls = walls',
+      ship = ship'
+    }
 
 
-rotateShip : Int -> Float -> Model -> Model
+rotateShip : Int -> Float -> Ship -> Ship
 rotateShip x dt ship = if x == -1 then
                           { ship | a = ship.a + (dt * 0.1) }
                        else
                           ship
 
-thrust : Int -> Float -> Model -> Model
+thrust : Int -> Float -> Ship -> Ship
 thrust y dt ship = if y == 1 then
                      let
                        (xt', yt') = (getvecship ship)
@@ -67,19 +76,17 @@ thrust y dt ship = if y == 1 then
                    else
                      ship
 
-
-physics : Float -> Model -> Model
-physics dt ship =
-  { ship |
-      x = ship.x + dt * ship.vx,
-      y = ship.y + dt * ship.vy
-  }
-
-
+--physics : Float -> Float -> Float -> Positioned {} -> Positioned {}
+physics vx vy dt obj = { obj | x = obj.x + dt * vx, y = obj.y + dt * vy }
 
 -- VIEW
 
-drawShip : Model -> Form
+drawWall : Wall -> Form
+drawWall wall = rect (toFloat wall.w) (toFloat wall.h)
+                  |> filled (rgb 0 0 0)
+                  |> move (wall.x, wall.y)
+
+drawShip : Ship -> Form
 drawShip ship = group [
                   circle 20
                     |> filled (rgb 255 0 0)
@@ -92,26 +99,27 @@ drawShip ship = group [
                   |> move (ship.x, ship.y)
 
 view : (Int, Int) -> Model -> Element
-view (w',h') ship =
+view (w',h') model =
   let
     (w,h) = (toFloat w', toFloat h')
   in
     collage w' h'
-      [ rect w h
+      ([ rect w h
           |> filled (rgb 174 238 238),
-        drawShip ship,
-        toForm (show (getvecship ship))
+        drawShip model.ship,
+        toForm (show (getvecship model.ship))
           |> move (-100, -100),
-        toForm (show ship.a)
+        toForm (show model.ship.a)
           |> move (-100, -150)
-      ]
+      ] ++ (List.map drawWall model.walls)
+    )
 
 
 -- SIGNALS
 
 main : Signal Element
 main =
-  Signal.map2 view Window.dimensions (Signal.foldp update ship input)
+  Signal.map2 view Window.dimensions (Signal.foldp update model input)
 
 
 input : Signal (Float, Keys)
